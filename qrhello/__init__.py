24255723db863cad29e8d9b034aa0ae41e4319de
Tasso Mulzer <tasso.mulzer@beuth-hochschule.de>
2019-10-11 20:56:55 +0200

    DB-File nicht in /tmp/.

--- qrhello/__init__.py
+++ qrhello/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from qrhello.dbfoo import Sqlite
 
 app = Flask(__name__)
-db = Sqlite('/tmp/db.sqlite')
+db = Sqlite('/var/lib/qrhello/db.sqlite')
 
 from qrhello import routes
 
